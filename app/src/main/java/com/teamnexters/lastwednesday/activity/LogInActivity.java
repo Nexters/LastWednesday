8d0e4f02f0f92da04abafe35d9833e0cb77ddd2c
jjangjamin <jaeminim95@gmail.com>
2018-01-12 14:45:20 +0900

    #7-구글로그인

--- app/src/main/java/com/teamnexters/lastwednesday/activity/LogInActivity.java
+++ app/src/main/java/com/teamnexters/lastwednesday/activity/LogInActivity.java
@@ -1,4 +1,4 @@
-package com.teamnexters.lastwednesday;
+package com.teamnexters.lastwednesday.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -20,6 +20,7 @@ import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.common.api.Status;
+import com.teamnexters.lastwednesday.MainActivity;
 import com.teamnexters.lastwednesday.R;
 
 /**
@@ -27,11 +28,8 @@ import com.teamnexters.lastwednesday.R;
  */
 
 public class LogInActivity extends AppCompatActivity implements View.OnClickListener, GoogleApiClient.OnConnectionFailedListener {
-    private LinearLayout Prof_Section;
-    private Button SignOut;
+
     private SignInButton SignIn;
-    private TextView Name, Email;
-    private ImageView Prof_Pic;
     private GoogleApiClient googleApiClient;
     private static final int REQ_CODE = 9001;
 
@@ -39,87 +37,45 @@ public class LogInActivity extends AppCompatActivity implements View.OnClickList
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Prof_Section = (LinearLayout)findViewById(R.id.prof_section);
-        SignOut = (Button)findViewById(R.id.bn_logout);
+        setContentView(R.layout.activity_login);
         SignIn = (SignInButton)findViewById(R.id.bn_login);
-        Name = (TextView)findViewById(R.id.name);
-        Email = (TextView)findViewById(R.id.email);
-        Prof_Pic = (ImageView)findViewById(R.id.prof_pic);
-
         SignIn.setOnClickListener(this);
-        SignOut.setOnClickListener(this);
-        Prof_Section.setVisibility(View.GONE);
+        TextView textView = (TextView) SignIn.getChildAt(0);
+        textView.setText("Sign in with Google");
         GoogleSignInOptions signInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
         googleApiClient = new GoogleApiClient.Builder(this).enableAutoManage(this, this).addApi(Auth.GOOGLE_SIGN_IN_API,signInOptions).build();
-
-
     }
 
     @Override
     public void onClick(View v) {
-
-        switch (v.getId())
-        {
-            case R.id.bn_login:
                 signIn();
-                break;
-            case R.id.bn_logout:
-                signOut();
-                break;
-        }
     }
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
     }
 
     private void signIn(){
         Intent intent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
         startActivityForResult(intent, REQ_CODE);
+    }
 
-    }
-    private void signOut(){
-        Auth.GoogleSignInApi.signOut(googleApiClient).setResultCallback(new ResultCallback<Status>() {
-            @Override
-            public void onResult(@NonNull Status status) {
-                updateUI(false);
-            }
-        });
-    }
 
     private void handleResult(GoogleSignInResult result){
-
         if(result.isSuccess()){
             GoogleSignInAccount account = result.getSignInAccount();
-            String name = account.getDisplayName();
-            String email = account.getEmail();
-            String img_url = account.getPhotoUrl().toString();
-            Name.setText(name);
-            Email.setText(email);
-            Glide.with(this).load(img_url).into(Prof_Pic);
-            updateUI(true);
+            Intent intent = new Intent(LogInActivity.this, MainActivity.class);
+            LogInActivity.this.startActivity(intent);
+            finish();
         }else{
-            updateUI(false);
-        }
-    }
-
-    private void updateUI(boolean isLogin){
-        if(isLogin){
-            Prof_Section.setVisibility(View.VISIBLE);
-            SignIn.setVisibility(View.GONE);
-        }
-        else{
-            Prof_Section.setVisibility(View.GONE);
             SignIn.setVisibility(View.VISIBLE);
         }
     }
 
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
         if(requestCode==REQ_CODE){
             GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
             handleResult(result);
--- app/src/main/java/com/teamnexters/lastwednesday/fragment/LogInFragment.java
+++
@@ -1,8 +0,0 @@
-package com.teamnexters.lastwednesday.fragment;
-
-/**
- * Created by user on 2018-01-11.
- */
-
-public class LogInFragment {
-}
